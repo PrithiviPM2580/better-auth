ffa221a118b3c3affd9d4082b0ec264ef0518ef1
Prithivi <prithivi@users.noreply.github.com>
2025-12-23 21:01:40 +0545

    feat: add example environment configuration and update .gitignore to exclude .env file

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,20 +1,23 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb"
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import {bearer} from "better-auth/plugins"
 
 const mongoClient = new MongoClient(process.env.MONGODB_URI!);
 const db = mongoClient.db();
 
 const auth = betterAuth({
-    database: mongodbAdapter(db, {
-        client: mongoClient,
-    }),
-    emailAndPassword: {
-        enabled: true,
+	database: mongodbAdapter(db), // Remove the client option
+    plugins: [bearer()],
+	emailAndPassword: {
+		enabled: true,
+	},
+    advanced:{
+        disableOriginCheck:true
     },
-    secret: process.env.BETTER_AUTH_SECRET!,
-    baseURL: process.env.BETTER_AUTH_URL!,
-    basePath: "/api/v1/auth",
+	secret: process.env.BETTER_AUTH_SECRET!,
+	baseURL: process.env.BETTER_AUTH_URL!,
+	basePath: "/api/v1/auth",
 });
 
 export default auth;
